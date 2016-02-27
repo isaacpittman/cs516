29a49e094608d01d62bc2e3f5329e7004e6fb808
Isaac Pittman <isaacpittman@users.noreply.github.com>
2016-02-27 17:04:45 -0500

    Update initialize.c to match the version given by professor in demo

--- XINU/initialize.c
+++ XINU/initialize.c
@@ -6,39 +6,50 @@
 #include "sem.h"
 #include "sleep.h"
 #include "mem.h"
-#include <tty.h>
+//#include <tty.h>
 #include "q.h"
 #include "io.h"
-#include <disk.h>
-#include <network.h>
+//#include <disk.h>
+//#include <network.h>
 
-extern	int	main();			/* address of user's main prog	*/
+extern	void	main_fun();			/* address of user's main prog	*/
+extern  void    end_game();         /* end context, kill -> resched */
+extern  void    proc(int);          /* process created by main_fun  */
+extern  void    procA(int);         /* process created by main_fun */
+extern  void    procC(int, int);    /* process created by main_fun */
 
 /* Declarations of major kernel variables */
 
 struct	pentry	proctab[NPROC]; /* process table			*/
-int	nextproc;		/* next process slot to use in create	*/
+int     nextproc;               /* next process slot to use in create	*/
 struct	sentry	semaph[NSEM];	/* semaphore table			*/
-int	nextsem;		/* next semaphore slot to use in screate*/
-struct	qent	q[NQENT];	/* q table (see queue.c)		*/
-int	nextqueue;		/* next slot in q structure to use	*/
-int	*maxaddr;		/* max memory address (set by sizmem)	*/
+int     nextsem;                /* next semaphore slot to use in screate*/
+struct	qent	q[NQENT];       /* q table (see queue.c)		*/
+int     nextqueue;              /* next slot in q structure to use	*/
+int     *maxaddr;               /* max memory address (set by sizmem)	*/
 #ifdef	NDEVS
 struct	intmap	intmap[NDEVS];	/* interrupt dispatch table		*/
 #endif
-struct	mblock	memlist;	/* list of free memory blocks		*/
+struct	mblock	memlist;        /* list of free memory blocks		*/
 #ifdef	Ntty
-struct  tty     tty[Ntty];	/* SLU buffers and mode control		*/
+//struct  tty     tty[Ntty];	/* SLU buffers and mode control		*/
 #endif
+
+sigset_t    full_block, full_unblock;       /* Masks to block or unblock interrupts during critical sections */
+ucontext_t  posix_ctxt_init, end_game_ctxt; /* TODO: Comment */
 
 /* active system status */
 
-int	numproc;		/* number of live user processes	*/
-int	currpid;		/* id of currently running process	*/
-int	reboot = 0;		/* non-zero after first boot		*/
+int	numproc;                    /* number of live user processes	*/
+int	currpid;                    /* id of currently running process	*/
+int	reboot = 0;                 /* non-zero after first boot		*/
 
-int	rdyhead,rdytail;	/* head/tail of ready list (q indexes)	*/
-char	vers[] = VERSION;	/* Xinu version printed at startup	*/
+#ifdef  RTCLOCK
+extern int  clkruns;            /* TODO: Comment */
+#endif
+
+int	rdyhead,rdytail;            /* head/tail of ready list (q indexes)	*/
+char	vers[] = VERSION;       /* Xinu version printed at startup	*/
 
 /************************************************************************/
 /***				NOTE:				      ***/
@@ -58,39 +69,59 @@ char	vers[] = VERSION;	/* Xinu version printed at startup	*/
  *  nulluser  -- initialize system and become the null process (id==0)
  *------------------------------------------------------------------------
  */
-int nulluser()				/* babysit CPU when no one home */
-{
-	int	userpid;
-	char	ps;
 
+int up=0;
+
+LOCAL   sysinit();
+void    idle_thread();
+void    main_fun(int);
+
+//int nulluser()				/* babysit CPU when no one home */
+int main(int argc, char *argv[])
+{
+    int     userpid, main_fun_cnt;  /* TODO: comment */
+    sigset_t ps;    /* TODO: Comment */
+
+    main_fun_cnt = 1;
+
+    sigfillset(&full_block);
+    sigemptyset(&full_unblock);
+
+/*
 	kprintf("\n\nXinu Version %s", vers);
 	if (reboot++ < 1)
 		kprintf("\n");
 	else
 		kprintf("   (reboot %d)\n", reboot);
+*/
+    /* TODO: Comment */
+    if(getcontext(&posix_ctxt_init) == -1){
+        perror("getcontext failed in initialize.c");
+        exit(5);
+    }
 	sysinit();			/* initialize all of Xinu */
+
+    end_game_ctxt = posix_ctxt_init;
+    end_game_ctxt.uc_stack.ss_sp    = (void *)((int)getstk(MINSTK)-MINSTK+1); /* TODO: Comment */
+    end_game_ctxt.uc_stack.ss_size  = MINSTK;
+    end_game_ctxt.uc_stack.ss_flags = 0;
+    makecontext(&end_game_ctxt, end_game, 0);
+
+/*
 	kprintf("%u real mem\n",(unsigned)maxaddr+(unsigned)sizeof(int));
 	kprintf("%u avail mem\n",
 		(unsigned)maxaddr-(unsigned)(&end)+(unsigned)sizeof(int));
 	kprintf("clock %sabled\n\n", clkruns==1?"en":"dis");
-	enable();			/* enable interrupts */
-
+*/
 	/* create a process to execute the user's main program */
 
-	userpid = create(main,INITSTK,INITPRIO,INITNAME,INITARGS);
+    /* TODO: Comment */
+    up = userpid = create(main_fun,INITSTK,INITPRIO,INITNAME,INITARGSC, main_fun_cnt);
 
-#ifdef	NETDAEMON
-	/* start the network input daemon process */
-	resume(
-	  create(NETIN, NETISTK, NETIPRI, NETINAM, NETIARGC, userpid)
-	);
-#else
-	resume( userpid );
-#endif
+    /* TODO: Comment */
+    setcontext (&(proctab[NULLPROC].posix_ctxt));
 
-	while (TRUE) {			/* run forever without actually */
-		pause();		/*  executing instructions	*/
-	}
+    write(1, "\BAD THINGS ARE HAPPENING\n", 26);
 	return;				/* unreachable			*/
 }
 
@@ -111,9 +142,14 @@ LOCAL	sysinit()
 	nextqueue= NPROC;		/* q[0..NPROC-1] are processes */
 
 	memlist.mnext = mptr =		/* initialize free memory list */
-	  (struct mblock *) roundew(&end);
+      (struct mblock *) malloc(FREE_SIZE);
+    if(mptr == NULL) {
+        perror("mallo failed ");
+        exit(3);
+    }
 	mptr->mnext = (struct mblock *)NULL;
-	mptr->mlen = truncew((unsigned)maxaddr-NULLSTK-(unsigned)&end);
+//	mptr->mlen = truncew((unsigned)maxaddr-NULLSTK-(unsigned)&end);
+    mptr->mlen = truckew((FREE_SIZE)-(NULLSTK)); /* TODO: Comment */
 
 	for (i=0 ; i<NPROC ; i++)	/* initialize process table */
 		proctab[i].pstate = PRFREE;
@@ -121,13 +157,30 @@ LOCAL	sysinit()
 	pptr = &proctab[NULLPROC];	/* initialize null process entry */
 	pptr->pstate = PRCURR;
 	pptr->pprio = 0;
-	strcpy(pptr->pname, "prnull");
-	pptr->plimit = ( (int)maxaddr ) - NULLSTK - sizeof(int);
-	pptr->pbase = maxaddr;
-	*( (int *)pptr->pbase ) = MAGIC;
-	pptr->paddr = nulluser;
+    /* TODO: Comment */
+    for(i=0; i<7; ++i){
+        pptr->pname[i] = *("prnull" +i);
+    }
+    //pptr->plimit = ( (int)maxaddr ) - NULLSTK - sizeof(int);
+    pptr->plimit = ( (int)mptr + (FREE_SIZE) - (NULLSTK) - 1); /* TODO: Comment */
+    //pptr->pbase = maxaddr;
+    // last byte address in pbase, but need an int address
+    // 4 bytes away from the end for hte following MAGIC assignment
+    // TODO: Clarify MAGIC
+    pptr->pbase = (int)mptr + (FREE_SIZE) - 1; /* TODO: Comment */
+    *( (int *)(pptr->pbase - 3) ) = MAGIC;
+    pptr->paddr = idle_thread;
 	pptr->phasmsg = FALSE;
 	pptr->pargs = 0;
+    pptr->posix_ctxt = posix_ctxt_init; /* TODO: Comment */
+
+    /* TODO: Comment */
+    pptr->posix_ctxt.uc_stack.ss_sp     = (void *)pptr->plimit;
+    pptr->posix_ctxt.uc_stack.ss_size   =   NULLSTK;
+    pptr->posix_ctxt.ss_flags           = 0;
+    pptr->posix_ctxt.uc_link            = &end_game_ctxt;
+
+    makecontext(&(pptr->posix_ctxt), idle_thread, 0);
 	currpid = NULLPROC;
 
 	for (i=0 ; i<NSEM ; i++) {	/* initialize semaphores */
@@ -147,7 +200,157 @@ LOCAL	sysinit()
 	dskdbp= mkpool(DBUFSIZ,NDBUFF);	/* initialize disk buffers */
 	dskrbp= mkpool(DREQSIZ,NDREQ);
 #endif
-	for ( i=0 ; i<NDEVS ; i++ )	/* initialize devices */
-		init(i);
+
+    // for ( i=0 ; i<NDEVS ; i++ )	/* initialize devices */
+    //	init(i);
+
 	return(OK);
+}
+
+void idle_thread(){
+    int i=1, j;
+    enable();   /* enable interrupts */
+    while(1) {
+        write(1, "#", 1);
+        if(i){
+            resume(up);
+            i=0;
+        }
+        resched();
+        for(j=0; j<100000000; ++j);
+    }
+}
+
+void main_fun(int arg){
+
+    int i, j=0, k, gig_count, lcount;
+    char *name = "tproc";
+    int sem1, pidA, pidB, pidC, main_pid;
+
+    main_pid = getpid();
+    write(1, "\nMAIN: MAIN FUNCTION IS ALIVE\N", 30);
+
+    if((sem1 = screate(0)) == SYSERR){
+        write(1, "\nsem create failed\n", 19);
+    }
+
+    if((pidA = create(procA, MINSTK, 20, name, 1, sem1)) == SYSERR){
+        write(1, "\ncreate A failed]n", 17);
+    }
+    if((pidB = create(procB, MINSTK, 20, name, 2, sem1, pidA)) == SYSERR){
+        write(1, "\ncreate B failed\n", 17);
+    }
+    if((pidC = create(procC, MINSTK, 20, name, 2, main_pid, 30)) == SYSERR){
+        write(1, "\ncreate C failed\n", 17);
+    }
+
+    write(1, "\nMAIN: main function has created procs A B and C\n",49);
+    write(1, "\nMAIN: main function about to suspend self\n", 43);
+
+    if(resume(pidA) == SYSERR ||
+       resume(pidB) == SYSERR ||
+       resume(pidC) == SYSERR){
+        write(1, "\nmain resume of A B and C failed\n", 33);
+    }
+
+    if(suspend(main_pid) == SYSERR){
+        write(1, "\nmain suspend failed\n", 21);
+    }
+    write(1, "\nMAIN: main function is awak and counting\n", 43);
+    gig_count = lcount = 0;
+    while(gig_count < 5){
+        if(++lcount < 0){
+            ++gig_count;
+            lcount=0;
+        }
+    }
+    write(1, "\nMAIN: main function finished, goodbye\n", 39);
+}
+
+void end_game(){
+    write(1,"<>",2);
+    kill(getpid());
+}
+
+void procA(int arg){
+    write(1, "\nA: process A is alive\n", 22);
+    write(1, "\nA: process A is about to wait on a sem\n", 39);
+
+    if(wait(arg) == SYSERR){
+        write(1, "\nin A wait failed\n", 18);
+    }
+
+    write(1, "\nA: process A is awake from sem, will wait for msg\n", 51);
+
+    if(receive() == SYSERR){
+        write(1, "\nin A receive failed\n", 21);
+    }
+
+    write(1, "\nA: process A has received a msg, goodbye\n", 42);
+
+}
+
+void procB(int arg1, int arg2){
+    write(1, "\nB: process B is alive\n", 22);
+    write(1, "\nB: process B is about to sleep for 2 seconds\n", 45);
+
+    if(sleep(2) == SYSERR){
+        write(1, "\nin B sleep failed\n", 19);
+    }
+
+    write(1, "\nB: process B is awake from sleep, will signal sem\n", 51);
+
+    if(signal(arg1) == SYSERR){
+        write(1, "\nin B signal failed\n", 20);
+    }
+    write(1, "\nB: process B is again to sleep for 2 seconds\n", 45);
+
+    if(sleep(2) == SYSERR){
+        write(1, "\nin B sleep2 failed\n", 20);
+    }
+
+    write(1, "\nB: process B is awake from sleep, will send msg to A\n", 54);
+
+    if(send(arg2, 111) == SYSERR){
+        write(1, "\nin B msg send failed\n",22);
+    }
+
+    write(1, "\nB: process B is about to sleep for 2 seconds\n", 45);
+
+    if(sleep(2) == SYSERR){
+        write(1, "\nin B sleep3 failed\n", 20);
+    }
+
+    write(1, "\nB: process B is awake from sleep, goodbye\n", 43);
+
+}
+
+void procC(int arg1, int arg2){
+    write(1, "\nC: process C is alive\n", 22);
+    write(1, "\nC: process C is about to sleep for 30 seconds\n", 48);
+
+    if(sleep(arg2) == SYSERR){
+        write(1, "\nin C sleep failed\n", 19);
+    }
+
+    write(1, "\nC: process C is awake from sleep, will resume main\n", 52);
+
+    if(resume(arg1) == SYSERR){
+        write(1, "\nin C resume failed\n", 20);
+    }
+
+    write(1, "\nC: process C is finished, goodbye\n", 35);
+}
+
+void proc(int arg){
+    int i, j=0;
+    char msg[] = {' ','P','R','O','C','\0',' '};
+
+    msg[5] = 48+arg;
+    write(1,msg,7);
+    if(arg ==4)sleep(2);
+    for(i=1; i<100000001; ++i){
+        if((i+1)%25000000 == 0)write(1, &msg[5], 1);
+    }
+    write(1,msg,7);
 }
