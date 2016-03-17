ddc5e2eabf46b3ca3fdba1c4ab7838e9ebbe9763
Isaac Pittman <isaacpittman@users.noreply.github.com>
2016-03-17 14:20:47 -0400

    Fix bug where sleep delta queue was not updated when sleeping pid was removed

--- XINU/kill.c
+++ XINU/kill.c
@@ -6,6 +6,8 @@
 #include "sem.h"
 #include "mem.h"
 #include "io.h"
+#include "q.h"
+#include "sleep.h"
 
 /*------------------------------------------------------------------------
  * kill  --  kill a process and remove it from the system
@@ -14,7 +16,7 @@
 SYSCALL kill(int pid)			/* process to kill              */
 {
 	struct	pentry	*pptr;		/* points to proc. table for pid*/
-	char	ps;
+    char	ps;
 
 	disable(ps);
 	if (isbadpid(pid) || (pptr= &proctab[pid])->pstate==PRFREE) {
@@ -24,13 +26,24 @@ SYSCALL kill(int pid)			/* process to kill              */
 	freestk(pptr->pbase, pptr->pstklen);
 	switch (pptr->pstate) {
 
-	  case PRCURR:	pptr->pstate = PRFREE;	/* suicide */
-			resched();
+      case PRCURR:
+        pptr->pstate = PRFREE;	/* suicide */
+        resched();
 
-	  case PRWAIT:	semaph[pptr->psem].semcnt++;
-						/* fall through */
-	  case PRSLEEP:
-	  case PRREADY:	dequeue(pid);
+      case PRWAIT:
+        semaph[pptr->psem].semcnt++;
+        dequeue(pid);
+        pptr->pstate = PRFREE;
+        break;
+
+      case PRSLEEP:
+        // Add my wait time to the next process in the delta queue's wait time
+        if (q[pid].qnext < NPROC){
+            q[q[pid].qnext].qkey += q[pid].qkey;
+        }
+
+      case PRREADY:
+        dequeue(pid);
 
 	  default:	pptr->pstate = PRFREE;
 	}
