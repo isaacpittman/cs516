8840cda13e11783a33b2f6dd007bf844418aeaf0
Isaac Pittman <isaacpittman@users.noreply.github.com>
2016-03-27 16:11:24 -0400

    Add command line interpreter header file, which includes token definitions.

---
+++ XINU/cli.h
@@ -0,0 +1,9 @@
+/* Define the tokens used by the the Command Line Interpreter's lexical analyzer. */
+#define TKN_SHOW_PROC   0
+#define TKN_SHOW_SLP    1
+#define TKN_RESUME      2
+#define TKN_KILL        3
+#define TKN_EXIT        4
+#define TKN_CREATE_SLP  5
+#define TKN_CREATE_RCV  6
+#define TKN_CREATE_WTR  7
