298a70a13e2b6081459f2b3cf45df9f8d5fc0e02
Isaac Pittman <isaacpittman@users.noreply.github.com>
2016-04-23 20:37:41 -0400

    Update to use XINU tty device for console read/write instead of Linux system calls: - Uses shared memory to simulate XINU's control status registers, since we don't have PDP11 hardware. - Includes signal handler to clean up shared memory - Removes write during idle thread, because the write could potentially block while waiting for the output buffer, and if idle thread is not on the ready queue when resched is called, then the ready queue could potentially be empty, causing a system crash. - Replace calls to write to stdin with writes to CONSOLE (our TTY device).

--- XINU/initialize.c
+++ XINU/initialize.c
@@ -12,6 +12,9 @@
 #include "io.h"
 //#include <disk.h>
 //#include <network.h>
+#include "slu.h"
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
 extern	void	main_fun();			/* address of user's main prog	*/
 extern  void    end_game();         /* end context, kill -> resched */
@@ -36,6 +39,9 @@ struct	mblock	memlist;        /* list of free memory blocks		*/
 struct  tty     tty[Ntty];	/* SLU buffers and mode control		*/
 #endif
 
+#define  SHKEY           15612415   /* shared memory segment shared with UART emulator */
+struct csr *tty0_csr; /* Need to simulate the control status registers */
+
 sigset_t    full_block, full_unblock;       /* Masks to block or unblock interrupts during critical sections */
 ucontext_t  posix_ctxt_init, end_game_ctxt; /* TODO: Comment */
 
@@ -44,6 +50,7 @@ ucontext_t  posix_ctxt_init, end_game_ctxt; /* TODO: Comment */
 int	numproc;                    /* number of live user processes	*/
 int	currpid;                    /* id of currently running process	*/
 int	reboot = 0;                 /* non-zero after first boot		*/
+int shmid;                      /* shared memory segment to emulate CSR */
 
 #ifdef  RTCLOCK
 extern int  clkruns;            /* TODO: Comment */
@@ -76,6 +83,7 @@ int up=0;
 LOCAL   sysinit();
 void    idle_thread();
 void    main_fun(int);
+void    term_mgr(int);
 
 //int nulluser()				/* babysit CPU when no one home */
 int main(int argc, char *argv[])
@@ -99,11 +107,47 @@ int main(int argc, char *argv[])
 	else
 		kprintf("   (reboot %d)\n", reboot);
 */
+
+    /* Set up a signal handler so we can removed shared memory */
+    struct        sigaction cleanup;
+    sigset_t      mask_sigs;
+    int           i,j,k, nsigs;
+    int sigs[] =  {SIGHUP,SIGINT,SIGQUIT,
+                   SIGSEGV, SIGBUS, SIGTERM, SIGXFSZ};
+    nsigs = sizeof(sigs)/sizeof(int);
+    for(i=0; i< nsigs; i++)
+          sigaddset(&mask_sigs, sigs[i]);
+     for(i=0; i< nsigs; i++){
+         cleanup.sa_handler = term_mgr;
+         cleanup.sa_mask = mask_sigs;
+         cleanup.sa_flags = SA_RESTART;
+         if(sigaction(sigs[i], &cleanup, NULL) == -1){
+            perror("can't set signals: ");
+            exit(1);
+         }
+     }
+
     /* TODO: Comment */
     if(getcontext(&posix_ctxt_init) == -1){
         perror("getcontext failed in initialize.c");
         exit(5);
     }
+    /* Need to get shared memory to simulate csr, since we don't have it in hardware */
+    if((shmid = shmget(SHKEY, 4096, IPC_CREAT | 0600)) == -1){
+        perror("shmget error");
+        exit(1);
+    }
+    if((tty0_csr = shmat(shmid, 0, 0)) == (struct csr *)-1){
+        perror("shmat error");
+        term_mgr(-1);
+    }
+
+    /* Initialize CSR */
+    tty0_csr->crbuf=0;
+    tty0_csr->ctbuf=0;
+    tty0_csr->crstat=SLUENABLE;
+
+    devtab[CONSOLE].dvcsr=tty0_csr;
 
     end_game_ctxt=posix_ctxt_init;
 
@@ -128,8 +172,15 @@ int main(int argc, char *argv[])
     /* TODO: Comment */
     setcontext (&(proctab[NULLPROC].posix_ctxt));
 
-    write(1, "\nBAD THINGS ARE HAPPENING\n", 26);
+    write(CONSOLE, "\nBAD THINGS ARE HAPPENING\n", 26);
 	return;				/* unreachable			*/
+}
+
+void term_mgr(int signal){
+    write(CONSOLE, "\nXinu exiting\n",14);
+    /* Remove shared memory */
+    shmctl(shmid, IPC_RMID, 0);
+    exit(1);
 }
 
 /*------------------------------------------------------------------------
@@ -208,8 +259,8 @@ LOCAL	sysinit()
 	dskrbp= mkpool(DREQSIZ,NDREQ);
 #endif
 
-    // for ( i=0 ; i<NDEVS ; i++ )	/* initialize devices */
-    //	init(i);
+    for ( i=0 ; i<NDEVS ; i++ )	/* initialize devices */
+        init(i);
 
 	return(OK);
 }
@@ -218,11 +269,8 @@ void idle_thread(){
     int i=1, j;
     enable();   /* enable interrupts */
     while(1) {
-        write(1, "#", 1);
         if(i){
-            if (resume(up) == SYSERR) {
-                  write(1, "\ncreate 0 failed\n", 17);
-            }
+            resume(up);
             i=0;
         }
         resched();
@@ -237,123 +285,123 @@ void main_fun(int arg){
     int sem1, pidA, pidB, pidC, main_pid;
 
     main_pid = getpid();
-    write(1, "\nINIT: MAIN FUNCTION IS ALIVE\n", 30);
+    write(CONSOLE, "\nINIT: MAIN FUNCTION IS ALIVE\n", 30);
 
     if((sem1 = screate(0)) == SYSERR){
-        write(1, "\nsem create failed\n", 19);
+        write(CONSOLE, "\nsem create failed\n", 19);
     }
 
     if((pidA = create(procA, MINSTK, 20, name, 1, sem1)) == SYSERR){
-        write(1, "\ncreate A failed\n", 17);
+        write(CONSOLE, "\ncreate A failed\n", 17);
     }
     if((pidB = create(procB, MINSTK, 20, name, 2, sem1, pidA)) == SYSERR){
-        write(1, "\ncreate B failed\n", 17);
+        write(CONSOLE, "\ncreate B failed\n", 17);
     }
     if((pidC = create(procC, MINSTK, 20, name, 2, main_pid, 30)) == SYSERR){
-        write(1, "\ncreate C failed\n", 17);
+        write(CONSOLE, "\ncreate C failed\n", 17);
     }
 
-    write(1, "\nINIT: main function has created procs A B and C\n",49);
-    write(1, "\nINIT: main function about to suspend self\n", 43);
+    write(CONSOLE, "\nINIT: main function has created procs A B and C\n",49);
+    write(CONSOLE, "\nINIT: main function about to suspend self\n", 43);
 
     if(resume(pidA) == SYSERR ||
        resume(pidB) == SYSERR ||
        resume(pidC) == SYSERR){
-        write(1, "\nmain resume of A B and C failed\n", 33);
+        write(CONSOLE, "\nmain resume of A B and C failed\n", 33);
     }
 
     if(suspend(main_pid) == SYSERR){
-        write(1, "\nmain suspend failed\n", 21);
+        write(CONSOLE, "\nmain suspend failed\n", 21);
     }
-    write(1, "\nINIT: main function has been resumed\n", 38);
+    write(CONSOLE, "\nINIT: main function has been resumed\n", 38);
 
     for (i=0; i<NPROC; ++i) {
         if (i==main_pid || i==NULLPROC)
             continue;
         if (proctab[i].pstate != PRFREE) {
-            write(1,"\nSome proc is still alive. Sleeping main for 5...\n", 50);
+//            write(CONSOLE,"\nSome proc is still alive. Sleeping main for 5...\n", 50);
             sleep(5);
             i=0;
         }
     }
 
-    write(1, "\nINIT: proc table empty, goodbye\n", 33);
+    write(CONSOLE, "\nINIT: proc table empty, goodbye\n", 33);
 
 }
 
 void end_game(){
-    write(1,"<>",2);
+    write(CONSOLE,"<>",2);
     kill(getpid());
 }
 
 void procA(int arg){
-    write(1, "\nA: process A is alive\n", 22);
-    write(1, "\nA: process A is about to wait on a sem\n", 39);
+    write(CONSOLE, "\nA: process A is alive\n", 22);
+    write(CONSOLE, "\nA: process A is about to wait on a sem\n", 39);
 
     if(wait(arg) == SYSERR){
-        write(1, "\nin A wait failed\n", 18);
+        write(CONSOLE, "\nin A wait failed\n", 18);
     }
 
-    write(1, "\nA: process A is awake from sem, will wait for msg\n", 51);
+    write(CONSOLE, "\nA: process A is awake from sem, will wait for msg\n", 51);
 
     if(receive() == SYSERR){
-        write(1, "\nin A receive failed\n", 21);
+        write(CONSOLE, "\nin A receive failed\n", 21);
     }
 
-    write(1, "\nA: process A has received a msg, goodbye\n", 42);
+    write(CONSOLE, "\nA: process A has received a msg, goodbye\n", 42);
 
 }
 
 void procB(int arg1, int arg2){
-    write(1, "\nB: process B is alive\n", 22);
-    write(1, "\nB: process B is about to sleep for 2 seconds\n", 45);
+    write(CONSOLE, "\nB: process B is alive\n", 22);
+    write(CONSOLE, "\nB: process B is about to sleep for 2 seconds\n", 45);
 
     if(sleep(2) == SYSERR){
-        write(1, "\nin B sleep failed\n", 19);
+        write(CONSOLE, "\nin B sleep failed\n", 19);
     }
 
-    write(1, "\nB: process B is awake from sleep, will signal sem\n", 51);
+    write(CONSOLE, "\nB: process B is awake from sleep, will signal sem\n", 51);
 
     if(signal(arg1) == SYSERR){
-        write(1, "\nin B signal failed\n", 20);
+        write(CONSOLE, "\nin B signal failed\n", 20);
     }
-    write(1, "\nB: process B is again to sleep for 2 seconds\n", 45);
+    write(CONSOLE, "\nB: process B is again to sleep for 2 seconds\n", 45);
 
     if(sleep(2) == SYSERR){
-        write(1, "\nin B sleep2 failed\n", 20);
+        write(CONSOLE, "\nin B sleep2 failed\n", 20);
     }
 
-    write(1, "\nB: process B is awake from sleep, will send msg to A\n", 54);
+    write(CONSOLE, "\nB: process B is awake from sleep, will send msg to A\n", 54);
 
     if(send(arg2, 111) == SYSERR){
-        write(1, "\nin B msg send failed\n",22);
+        write(CONSOLE, "\nin B msg send failed\n",22);
     }
 
-    write(1, "\nB: process B is about to sleep for 2 seconds\n", 45);
+    write(CONSOLE, "\nB: process B is about to sleep for 2 seconds\n", 45);
 
     if(sleep(2) == SYSERR){
-        write(1, "\nin B sleep3 failed\n", 20);
+        write(CONSOLE, "\nin B sleep3 failed\n", 20);
     }
 
-    write(1, "\nB: process B is awake from sleep, goodbye\n", 43);
+    write(CONSOLE, "\nB: process B is awake from sleep, goodbye\n", 43);
 
 }
 
 void procC(int arg1, int arg2){
-    write(1, "\nC: process C is alive\n", 22);
-    write(1, "\nC: process C is about to sleep for 30 seconds\n", 48);
+    write(CONSOLE, "\nC: process C is alive\n", 22);
+    write(CONSOLE, "\nC: process C is about to sleep for 30 seconds\n", 48);
 
     if(sleep(30) == SYSERR){
-        write(1, "\nin C sleep failed\n", 19);
+        write(CONSOLE, "\nin C sleep failed\n", 19);
     }
 
-    write(1, "\nC: process C is awake from sleep, will resume INIT\n", 52);
+    write(CONSOLE, "\nC: process C is awake from sleep, will resume INIT\n", 52);
 
     if(resume(arg1) == SYSERR){
-        write(1, "\nin C resume failed\n", 20);
+        write(CONSOLE, "\nin C resume failed\n", 20);
     }
 
-    write(1, "\nC: process C is starting the CLI\n", 34);
+    write(CONSOLE, "\nC: process C is starting the CLI\n", 34);
 
     start_cli();
 }
