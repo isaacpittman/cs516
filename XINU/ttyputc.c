65997b55b5f91cf2c277d116e76ae71fd90bb9d2
Isaac Pittman <isaacpittman@users.noreply.github.com>
2016-04-23 20:48:55 -0400

    Add error handling to the wait call. Change include from <> to "" to ensure local file is included instead of system library.

--- XINU/ttyputc.c
+++ XINU/ttyputc.c
@@ -1,10 +1,10 @@
 /* ttyputc.c - ttyputc */
 
-#include <conf.h>
-#include <kernel.h>
-#include <tty.h>
-#include <io.h>
-#include <slu.h>
+#include "conf.h"
+#include "kernel.h"
+#include "tty.h"
+#include "io.h"
+#include "slu.h"
 
 /*------------------------------------------------------------------------
  *  ttyputc - write one character to a tty device
@@ -16,9 +16,13 @@ int ttyputc(struct devsw *devptr, char ch)
     sigset_t	ps;
 
 	iptr = &tty[devptr->dvminor];
-        if ( ch==NEWLINE && iptr->ocrlf )
-                ttyputc(devptr,RETURN);
-	wait(iptr->osem);		/* wait	for space in queue	*/
+    if ( ch==NEWLINE && iptr->ocrlf ){
+        ttyputc(devptr,RETURN);
+    }
+    /* wait	for space in queue	*/
+    if (wait(iptr->osem)==SYSERR){
+        exit(5);
+    }
 	disable(ps);
 	iptr->obuff[iptr->ohead++] = ch;
 	if (iptr->ohead	>= OBUFLEN)
