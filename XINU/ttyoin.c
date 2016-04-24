3272d8263221f33ce78e3372f6aea0ee38495e7d
Isaac Pittman <isaacpittman@users.noreply.github.com>
2016-04-23 20:47:27 -0400

    Set SLUREADYON bit manually. Normally, this would be set by PDP11 hardware when output is sent to the buffer, but we don't have PDP11 hardware. Remove register hints. Change <> to "" in includes to ensure local file is included instead of system library.

--- XINU/ttyoin.c
+++ XINU/ttyoin.c
@@ -1,18 +1,18 @@
 /* ttyoin.c - ttyoin */
 
-#include <conf.h>
-#include <kernel.h>
-#include <tty.h>
-#include <io.h>
-#include <slu.h>
+#include "conf.h"
+#include "kernel.h"
+#include "tty.h"
+#include "io.h"
+#include "slu.h"
 
 /*------------------------------------------------------------------------
  *  ttyoin  --  lower-half tty device driver for output interrupts
  *------------------------------------------------------------------------
  */
-void	ttyoin(register struct tty *iptr)
+void	ttyoin(struct tty *iptr)
 {
-	register struct	csr	*cptr;
+    struct	csr	*cptr;
 	int	ct;
 
 	cptr = iptr->ioaddr;
@@ -20,6 +20,7 @@ void	ttyoin(register struct tty *iptr)
 		cptr->ctbuf = iptr->ebuff[iptr->etail++];
 		if (iptr->etail	>= EBUFLEN)
 			iptr->etail = 0;
+        cptr->ctstat = cptr->ctstat | SLUREADYON; // We have to "set the interrupt bit", because we don't have hardware to set it automatically.
 		return;
 	}
 	if (iptr->oheld) {			/* honor flow control	*/
@@ -29,13 +30,14 @@ void	ttyoin(register struct tty *iptr)
 	if ((ct=scount(iptr->osem)) < OBUFLEN) {
 		cptr->ctbuf = iptr->obuff[iptr->otail++];
 		if (iptr->otail	>= OBUFLEN)
-			iptr->otail = 0;
+            iptr->otail = 0;
 		if (ct > OBMINSP)
 			signal(iptr->osem);
 		else if	( ++(iptr->odsend) == OBMINSP) {
 			iptr->odsend = 0;
 			signaln(iptr->osem, OBMINSP);
 		}
+        cptr->ctstat = cptr->ctstat | SLUREADYON; // We have to "set the interrupt bit", because we don't have hardware to set it automatically.
 	} else
 		cptr->ctstat = SLUDISABLE;
 	return;
